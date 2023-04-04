86225b7abc38cfe690085105b3ab78e6488451e6
Brenden Moore <brendenm3603@gmail.com>
2023-04-03 21:01:46 -0700

    added extra tests

--- python/tests/data_structures/test_linked_list.py
+++ python/tests/data_structures/test_linked_list.py
@@ -73,3 +73,37 @@ def test_includes_false():
     linked_list.insert("banana")
 
     assert not linked_list.includes("cucumber")
+
+
+# extra tests
+
+def test_to_string_triple():
+    linked_list = LinkedList()
+
+    linked_list.insert("apple")
+
+    assert str(linked_list) == "{ apple } -> NULL"
+
+    linked_list.insert("banana")
+
+    assert str(linked_list) == "{ banana } -> { apple } -> NULL"
+
+    linked_list.insert("grape")
+
+    assert str(linked_list) == "{ grape } -> { banana } -> { apple } -> NULL"
+
+
+def test_includes_null():
+    linked_list = LinkedList()
+
+    linked_list.insert("NULL")
+
+    assert linked_list.includes("NULL")
+
+
+def test_insert_int():
+    linked_list = LinkedList()
+
+    linked_list.insert("0")
+
+    assert linked_list.includes("0")
