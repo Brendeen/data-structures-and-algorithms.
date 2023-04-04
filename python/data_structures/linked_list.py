47457fa8110a52a2906b7758abe0403dd420917e
Brenden Moore <brendenm3603@gmail.com>
2023-04-03 20:20:07 -0700

    completed code challenge 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,56 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def includes(self, value):
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next
+        return False
+
+    def __str__(self):
+        current_node = self.head
+        string = ""
+        if current_node is None:
+            return "NULL"
+        # elif current_node is self.head:
+        #     return "{ " + current_node.value + " } -> NULL"
+        # elif current_node is self.head.next:
+        #     return "{ " + current_node.value + " } -> { " + current_node.next.value + " } -> NULL"
+        else:
+            string = "{ " + current_node.value + " }"
+            current_node = current_node.next
+            while current_node:
+                string += " -> { " + current_node.value + " }"
+                current_node = current_node.next
+            string += " -> NULL"
+            return string
 
 
 class TargetError:
+    linked = LinkedList()
+
+
+if __name__ == "__main__":
+    # print(LinkedList().head)
+    # LinkedList().insert("apple")
+    # print(LinkedList().head)
     pass
+
