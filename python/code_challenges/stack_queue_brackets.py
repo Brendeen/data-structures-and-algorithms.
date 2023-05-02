19e33d7e43cc513e43a421d766395ac382dcb79d
Brenden Moore <brendenm3603@gmail.com>
2023-05-01 21:19:24 -0700

    added code challenge 13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,20 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    s = Stack()
+    opening_bracks = ['(', '{', '[']
+    closing_bracks = [')', '}', ']']
+
+    for character in string:
+        if character in string[0] == ')' or string[0] == '}' or string[0] == ']':
+            return False
+        if character in opening_bracks:
+            s.push(character)
+        elif character in closing_bracks:
+            if not s:
+                return False
+            if closing_bracks.index(character) == opening_bracks.index(s.pop()):
+                return True
+            else:
+                return False
