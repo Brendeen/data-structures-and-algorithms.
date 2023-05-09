cd3bd90ef7a624deb57c81ccc40d95e18173efdc
Brenden Moore <brendenm3603@gmail.com>
2023-05-08 21:06:22 -0700

    added data structure Binary tree & search tree

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -3,14 +3,54 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        result = []
+
+        def traverse(node):
+            if not node:
+                return
+
+            result.append(node.value)
+            traverse(node.left)
+            traverse(node.right)
+
+        traverse(self.root)
+        return result
+
+    def in_order(self):
+        result = []
+
+        def traverse(node):
+            if not node:
+                return
+
+            traverse(node.left)
+            result.append(node.value)
+            traverse(node.right)
+
+        traverse(self.root)
+        return result
+
+    def post_order(self):
+        result = []
+
+        def traverse(node):
+            if not node:
+                return
+
+            traverse(node.left)
+            traverse(node.right)
+            result.append(node.value)
+
+        traverse(self.root)
+        return result
 
 
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
