79ad814643f94a51c05201d64fe64cb70573f445
Brenden Moore <brendenm3603@gmail.com>
2023-05-11 20:48:38 -0700

    added code challenge 16 code

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -48,6 +48,26 @@ class BinaryTree:
         traverse(self.root)
         return result
 
+    def find_maximum_value(self):
+        if not self.root:
+            return None
+
+        max_val = self.root.value
+
+        def traverse(node):
+            nonlocal max_val
+            if not node:
+                return
+
+            if node.value > max_val:
+                max_val = node.value
+
+            traverse(node.left)
+            traverse(node.right)
+
+        traverse(self.root)
+        return max_val
+
 
 class Node:
     def __init__(self, value, left=None, right=None):
