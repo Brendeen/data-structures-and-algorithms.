7823466921015919c8dbb73e01610928967df83c
Brenden Moore <brendenm3603@gmail.com>
2023-07-03 21:57:15 -0700

    finished challenge 32

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,29 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree1, tree2):
+    values1 = {}
+    values2 = {}
+
+    def traverse(node, values):
+        if not node:
+            return
+
+        values[node.value] = True
+        traverse(node.left, values)
+        traverse(node.right, values)
+
+    traverse(tree1.root, values1)
+    traverse(tree2.root, values2)
+
+    common_values = []
+    for value in values1:
+        if value in values2:
+            common_values.append(value)
+
+    if common_values:
+        print(common_values)
+        return common_values
+    else:
+        print("No like values.")
+        return None
